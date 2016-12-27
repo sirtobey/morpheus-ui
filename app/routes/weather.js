423889ab72b42101fee68097f1d77c5d40bb1c90
Tobias Feller <tobiasmichaelfeller@gmail.com>
2016-12-27 22:26:41 +0100

    Add pseudo weather model on route

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [{
+      hour: '8:00',
+      temp: 8
+    }, {
+      hour: '9:00',
+      temp: 12
+    }]
+  }
 });
